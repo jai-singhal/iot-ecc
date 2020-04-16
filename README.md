c5fde426063e961902a5d3f7e6ce598caa99cdfa
Jai Singhal <jaisinghal48@gmail.com>
2020-04-16 13:17:11 +0530

    readme

--- README.md
+++ README.md
@@ -2,18 +2,3 @@
 https://demo.codimd.org/BZ5z32LXS4q31BTO_vwRSA
 
 uvicorn serverRSA:app --reload
-
-## TODOS
-we need to make sure we include:
-~~1> what is DH~~
-~~2> dh ecc~~
-~~3> small intro abt RSA~~
-4> ecc vs RSA
-
-5> practical
-
-
-## Slideshare to copy from
-- https://www.slideshare.net/BaraniTharan14/elliptical-curve-cryptography 
-- https://www.slideshare.net/KellyBresnahan/elliptic-curve-cryptography-66406021
-- 
--- src/visualize.py
+++ src/visualize.py
@@ -27,42 +27,82 @@ dbRSATime = TinyDB('../db/serverdbRSA.json',
     # storage=CachingMiddleware(JSONStorage)
 )
 
-data_info = []
-for row in dbECC:
-    data_info.append(row)
+def visualize_ecc():
+    data_info = []
+    for row in dbECC:
+        data_info.append(row)
 
-kdf = pd.DataFrame(data_info)
-del kdf["deviceid"]
-del kdf["longitude"]
-del kdf["latitude"]
-del kdf["curve_name"]
-del kdf["created_at"]
-del kdf["secretKey"]
+    kdf = pd.DataFrame(data_info)
+    del kdf["deviceid"]
+    del kdf["longitude"]
+    del kdf["latitude"]
+    del kdf["curve_name"]
+    del kdf["created_at"]
+    del kdf["secretKey"]
 
-avg_keygentime = kdf["keygen_time"].mean()
-avg_keygentime = '%.3f' %(avg_keygentime/10**6) + "ms"
+    avg_keygentime = kdf["keygen_time"].mean()
+    avg_keygentime = '%.3f' %(avg_keygentime/10**6) + "ms"
 
-data = []
-for row in dbECCData:
-    data.append(row)
+    data = []
+    for row in dbECCData:
+        data.append(row)
 
-df = pd.DataFrame(data)
-del df["transaction_id"]
-del df["filepath"]
-del df["keysize"]
+    df = pd.DataFrame(data)
+    del df["transaction_id"]
+    del df["filepath"]
+    del df["keysize"]
 
-meandf = df.groupby(["msg_len"]).mean()
-meandf.sort_values("msg_len")
-meandf["encrypt_time"] = meandf["encrypt_time"]/10**6
-meandf["decrypt_time"] = meandf["decrypt_time"]/10**6
-meandf["total_time"] = meandf["total_time"]/10**6
+    meandf = df.groupby(["msg_len"]).mean()
+    meandf.sort_values("msg_len")
+    meandf["encrypt_time"] = meandf["encrypt_time"]/10**6
+    meandf["decrypt_time"] = meandf["decrypt_time"]/10**6
+    meandf["total_time"] = meandf["total_time"]/10**6
 
-ax = meandf.plot(rot=0,  use_index=True, kind='bar', title='Time vs message size')
+    ax = meandf.plot(rot=0,  use_index=True, kind='bar', title='Time vs message size')
+    print("Avg Key-generation time: " + avg_keygentime)
+    plt.text(0, 30, "Avg Key-generation time: " + avg_keygentime)
 
-plt.text(0, 22, "Avg Key-generation time: " + avg_keygentime)
-plt.xlabel("Message Length (in KB)")
-plt.ylabel("Time required (in ms)")
-plt.legend(['Encryption time', 'Decryption time', 'Total time'])
-plt.show()
+    plt.xlabel("Message Length (in KB)")
+    plt.ylabel("Time required (in ms)")
+    plt.legend(['Encryption time', 'Decryption time', 'Total time'])
+
+    plt.show()
 
 
+
+def visualize_rsa():
+    data = []
+    for row in dbRSATime:
+        data.append(row)
+
+    df = pd.DataFrame(data)
+    del df["transaction_id"]
+    del df["device_id"]
+    del df["key_size"]
+    del df["plain_text_msg"]
+
+    total_time_list=[]
+    for index, i in df.iterrows():
+        total_time_list.append(i["encrypt_msg"]+i["decrypt_msg"]+i["key_gen_time"])
+
+    df['total_time']=total_time_list
+    df["msg_length"]=df["msg_length"]/1000
+
+    meandf = df.groupby(["msg_length"]).mean()
+    meandf.sort_values("msg_length")
+    meandf["key_gen_time"] = meandf["key_gen_time"]/10**6
+    meandf["encrypt_msg"] = meandf["encrypt_msg"]/10**6
+    meandf["decrypt_msg"] = meandf["decrypt_msg"]/10**6
+    meandf["total_time"] = meandf["total_time"]/10**6
+
+    ax = meandf.plot(rot=0,  use_index=True, kind='bar', title='Time vs message size')
+    plt.xlabel("Message Length (in KB)")
+    plt.ylabel("Time required (in ms)")
+    plt.legend(['keygen time','Encryption time', 'Decryption time', 'Total time'])
+    # ax.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    visualize_ecc()
+    visualize_rsa()
